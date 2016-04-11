f1394dbe70efeef624fa74aff2de211e563e472c
Michael Falkowski <mfalko2@gmail.com>
2016-04-10 23:58:01 -0500

    'nwzt'

--- src/Line.js
+++ src/Line.js
@@ -10,6 +10,7 @@ class Line {
         vertex.z = Math.random() * 4000 - 2000;
         this.noise = new noise();
         this.noise.seed(Math.random());
+
         geometry.vertices.push( vertex );
         geometry.colors.push( new THREE.Color( colors[ Math.floor( Math.random() * colors.length ) ] ) );
 
@@ -18,7 +19,8 @@ class Line {
         scene.add( this.mesh );
         this.nx = this.nxx = 0;
         this.ny = this.nyy = 0;
-        this.n = this.noise.perlin2(0,0);
+        this.n = this.noise.perlin2(10,5);
+
         this.vector = new THREE.Vector3( 1, 0, 0 );
         this.yAxis = new THREE.Vector3( 0, 1, 0 );
         this.xAxis = new THREE.Vector3( 1, 0, 0 );
@@ -33,17 +35,16 @@ class Line {
         var vec = new THREE.Vector3(this.x, 0, 1000)
         var time = Date.now() * 0.0005;
         
-        var stepSize = 0.0003
-        this.n += this.noise.perlin2(this.nx, this.ny) *360;
+        var stepSize = 0.001 + Math.random() * 0.015;
+        this.n += this.noise.perlin2(this.nx, this.ny) ;
         this.nx += stepSize
         this.ny += stepSize
         this.mesh.position.y += this.vector.y
         this.mesh.position.x += this.vector.x
-        
-        this.vector.applyAxisAngle( this.yAxis, this.angle );
-        this.vector.applyAxisAngle( this.xAxis, this.angle );
-        this.vector.applyAxisAngle( this.zAxis, this.angle );
-
+        this.mesh.position.z += this.vector.z
+        this.vector.applyAxisAngle( this.yAxis, this.n );
+        this.vector.applyAxisAngle( this.xAxis, this.n );
+        this.vector.applyAxisAngle( this.zAxis, this.n );
         // this.mesh.rotation.z = this.ny
        // this.mesh.rotation.z = this.n
 
